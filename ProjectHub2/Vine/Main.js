f61f8012440e61e6e4e6c133b3c1965036eed803
Vilius Vaiciulis <Vilius Vaiciulis>
2016-07-20 11:59:01 +0300

    updated VineService

--- ProjectHub2/Vine/Main.js
+++ ProjectHub2/Vine/Main.js
@@ -1,12 +1,7 @@
 var Hapi = require('hapi');
-var request = require('request');
+var VineService = require('./VineService.js');
 
 var server = new Hapi.Server();
-
-category = 1;
-type = 'popular';
-
-url = 'https://api.vineapp.com/timelines/channels/' + category + '/' + type;
 
 server.connection({
     host: 'localhost',
@@ -15,21 +10,8 @@ server.connection({
 
 server.route({
     method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply("done");
-    }
-});
-
-request({
-    url: url,
-    json: true
-}, function (error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-        console.log(JSON.stringify(body, null, 2)) // Print the json response
-    }
+    path: '/vine',
+    handler: VineService.getVines
 })
-
 
 server.start(function (){console.log('Server running at: ', server.info.uri)});
